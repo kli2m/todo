7733ce9ac27c104192c0fe67781a00ac69bb093d
Klimm Ivan <ivanklimm87@gmail.com>
2022-06-16 10:40:16 +0300

    refactor: Fixed ControlPanel

--- src/components/ControlPanel/ControlPanel.tsx
+++ src/components/ControlPanel/ControlPanel.tsx
@@ -1,29 +1,62 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './ControlPanel.scss';
 
-const ControlPanel = () => (
-  <div className="control">
-    <div className="control__info-mess">
-      <span>2 items left</span>
-    </div>
-    <div className="control__sort-box">
-      <div className="control__sort-box_item all">
-        <input type="radio" value="All" id="all" name="sort"></input>
-        <label htmlFor="all">All</label>
+const ControlPanel = () => {
+  const [toggle, setToggle] = useState<string>('All');
+
+  useEffect(() => {
+    console.log(toggle);
+  }, [toggle]);
+
+  const handleChangeToggle = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setToggle(event.target.value);
+  };
+
+  return (
+    <div className="control">
+      <div className="control__info-mess">
+        <span>2 items left</span>
       </div>
-      <div className="control__sort-box_item active">
-        <input type="radio" value="Active" id="active" name="sort"></input>
-        <label htmlFor="active">Active</label>
+      <div className="control__sort-box">
+        <div className="control__sort-box_item all">
+          <input
+            type="radio"
+            value="All"
+            id="all"
+            name="sort"
+            checked={toggle === 'All'}
+            onChange={handleChangeToggle}
+          ></input>
+          <label htmlFor="all">All</label>
+        </div>
+        <div className="control__sort-box_item active">
+          <input
+            type="radio"
+            value="Active"
+            id="active"
+            name="sort"
+            checked={toggle === 'Active'}
+            onChange={handleChangeToggle}
+          ></input>
+          <label htmlFor="active">Active</label>
+        </div>
+        <div className="control__sort-box_item completed">
+          <input
+            type="radio"
+            value="Completed"
+            id="completed"
+            name="sort"
+            checked={toggle === 'Completed'}
+            onChange={handleChangeToggle}
+          ></input>
+          <label htmlFor="completed">Completed</label>
+        </div>
       </div>
-      <div className="control__sort-box_item completed">
-        <input type="radio" value="Completed" id="completed" name="sort"></input>
-        <label htmlFor="completed">Completed</label>
+      <div className="control__clear">
+        <button className="control__clear_btn">Clear completed</button>
       </div>
     </div>
-    <div className="control__clear">
-      <button>Clear completed</button>
-    </div>
-  </div>
-);
+  );
+};
 
 export default ControlPanel;
